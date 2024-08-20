1c4620b6d055c2874e5a90d0d7ab5f31d240b3c1
hossam <hossameldeen0015@gmail.com>
2024-08-20 19:43:28 +0300

    dropdowen

---
+++ Controllers/CatgoryController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mvc_Project.Models.ViewModels;
+
+namespace Mvc_Project.Controllers
+{
+    public class CatgoryController : Controller
+    {
+
+        public IActionResult showCatgory(int id)
+        {
+            if(id == 1)
+            {
+                return View("Dresses");
+
+            }
+            if (id == 2)
+            {
+                return View("Tops");
+
+            }
+            if (id == 3)
+            {
+                return View("Bottoms");
+
+            }
+            if (id == 4)
+            {
+                return View("Shoes");
+
+            }
+            if (id == 5)
+            {
+                return View("AccessoriesAndMakeUp");
+
+            }
+            return View("index");
+
+        }
+    }
+}
